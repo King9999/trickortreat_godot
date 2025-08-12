531ac780dffeed3bdfe4e4452b8aa07a2480dfe0
Mike Murray <mikemurray056@gmail.com>
2025-08-12 16:15:34 -0400

    Added singleton
    The players' input now come from singleton. Need to save player's selections

--- Scripts/input_manager.gd
+++ Scripts/input_manager.gd
@@ -10,7 +10,7 @@ var input_p1: InputEvent
 var input_p2: InputEvent
 
 #player controls
-const left_p1: String = "P1_Left"
+"""const left_p1: String = "P1_Left"
 const right_p1: String = "P1_Right"
 const up_p1: String = "P1_Up"
 const down_p1: String = "P1_Down"
@@ -20,6 +20,7 @@ const right_p2: String = "P2_Right"
 const up_p2: String = "P2_Up"
 const down_p2: String = "P2_Down"
 const confirm_p2: String = "P2_Trick"
+"""
 
 #references to costume sprites
 enum Selection { GHOST, KNIGHT, PRINCESS, WITCH }
@@ -39,42 +40,40 @@ var x_offset_cursor2: Vector2 = Vector2(20, 0)
 func _ready() -> void:
 	costume_p1 = Selection.GHOST
 	costume_p2 = Selection.KNIGHT
-	pass # Replace with function body.
-
 
 
 func _process(delta: float) -> void:
 	#Player 1 controls
-	if Input.is_action_just_pressed(left_p1):	#is_action_just_pressed executes action only once
+	if Input.is_action_just_pressed(Singleton.left_p1):	#is_action_just_pressed executes action only once
 		#move P1 cursor left
 		if (costume_p1 <= 0):
 			costume_p1 = Selection.size() - 1
 		else:
 			costume_p1 -= 1		
 		print(Selection.keys()[costume_p1])
-	if Input.is_action_just_pressed(right_p1):
+	if Input.is_action_just_pressed(Singleton.right_p1):
 		if (costume_p1 >= Selection.size() - 1):
 			costume_p1 = 0
 		else:
 			costume_p1 += 1
 		print(Selection.keys()[costume_p1])
-	if Input.is_action_just_pressed(confirm_p1):
+	if Input.is_action_just_pressed(Singleton.confirm_p1):
 		print("P1 Selected {0}".format([Selection.keys()[costume_p1]]))
 	
 	#Player 2 controls
-	if Input.is_action_just_pressed(left_p2):
+	if Input.is_action_just_pressed(Singleton.left_p2):
 		if (costume_p2 <= 0):
 			costume_p2 = Selection.size() - 1
 		else:
 			costume_p2 -= 1
 		print("P2 Presed left")
-	if Input.is_action_just_pressed(right_p2):
+	if Input.is_action_just_pressed(Singleton.right_p2):
 		if (costume_p2 >= Selection.size() - 1):
 			costume_p2 = 0
 		else:
 			costume_p2 += 1
 		print("P2 Presed right")
-	if Input.is_action_just_pressed(confirm_p2):
+	if Input.is_action_just_pressed(Singleton.confirm_p2):
 		print("P2 Selected costume")
 	
 	#update cursor positions
---
+++ Scripts/singleton.gd
@@ -0,0 +1,23 @@
+extends Node
+
+#player controls
+const left_p1: String = "P1_Left"
+const right_p1: String = "P1_Right"
+const up_p1: String = "P1_Up"
+const down_p1: String = "P1_Down"
+const confirm_p1: String = "P1_Trick"
+const left_p2: String = "P2_Left"
+const right_p2: String = "P2_Right"
+const up_p2: String = "P2_Up"
+const down_p2: String = "P2_Down"
+const confirm_p2: String = "P2_Trick"
+
+var score: int = 100
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
