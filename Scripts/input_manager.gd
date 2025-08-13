302ea6b998defe5ca70d764f17a8ff260a395ec9
Mike Murray <mikemurray056@gmail.com>
2025-08-13 10:33:15 -0400

    Singleton now saves players' costume picks

--- Scripts/input_manager.gd
+++ Scripts/input_manager.gd
@@ -23,9 +23,9 @@ const confirm_p2: String = "P2_Trick"
 """
 
 #references to costume sprites
-enum Selection { GHOST, KNIGHT, PRINCESS, WITCH }
+"""enum Selection { GHOST, KNIGHT, PRINCESS, WITCH }
 @export var costume_p1: Selection
-@export var costume_p2: Selection
+@export var costume_p2: Selection"""
 @onready var ghost_sprite: Sprite2D = $"../Ghost"
 @onready var knight_sprite: Sprite2D = $"../Knight"
 @onready var princess_sprite: Sprite2D = $"../Princess"
@@ -38,66 +38,66 @@ var x_offset_cursor2: Vector2 = Vector2(20, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	costume_p1 = Selection.GHOST
-	costume_p2 = Selection.KNIGHT
+	Singleton.costume_p1 = Singleton.Selection.GHOST
+	Singleton.costume_p2 = Singleton.Selection.KNIGHT
 
 
 func _process(delta: float) -> void:
 	#Player 1 controls
 	if Input.is_action_just_pressed(Singleton.left_p1):	#is_action_just_pressed executes action only once
 		#move P1 cursor left
-		if (costume_p1 <= 0):
-			costume_p1 = Selection.size() - 1
+		if (Singleton.costume_p1 <= 0):
+			Singleton.costume_p1 = Singleton.Selection.size() - 1
 		else:
-			costume_p1 -= 1		
-		print(Selection.keys()[costume_p1])
+			Singleton.costume_p1 -= 1		
+		print(Singleton.Selection.keys()[Singleton.costume_p1])
 	if Input.is_action_just_pressed(Singleton.right_p1):
-		if (costume_p1 >= Selection.size() - 1):
-			costume_p1 = 0
+		if (Singleton.costume_p1 >= Singleton.Selection.size() - 1):
+			Singleton.costume_p1 = 0
 		else:
-			costume_p1 += 1
-		print(Selection.keys()[costume_p1])
+			Singleton.costume_p1 += 1
+		print(Singleton.Selection.keys()[Singleton.costume_p1])
 	if Input.is_action_just_pressed(Singleton.confirm_p1):
-		print("P1 Selected {0}".format([Selection.keys()[costume_p1]]))
+		print("P1 Selected {0}".format([Singleton.Selection.keys()[Singleton.costume_p1]]))
 	
 	#Player 2 controls
 	if Input.is_action_just_pressed(Singleton.left_p2):
-		if (costume_p2 <= 0):
-			costume_p2 = Selection.size() - 1
+		if (Singleton.costume_p2 <= 0):
+			Singleton.costume_p2 = Singleton.Selection.size() - 1
 		else:
-			costume_p2 -= 1
+			Singleton.costume_p2 -= 1
 		print("P2 Presed left")
 	if Input.is_action_just_pressed(Singleton.right_p2):
-		if (costume_p2 >= Selection.size() - 1):
-			costume_p2 = 0
+		if (Singleton.costume_p2 >= Singleton.Selection.size() - 1):
+			Singleton.costume_p2 = 0
 		else:
-			costume_p2 += 1
+			Singleton.costume_p2 += 1
 		print("P2 Presed right")
 	if Input.is_action_just_pressed(Singleton.confirm_p2):
 		print("P2 Selected costume")
 	
 	#update cursor positions
-	match (costume_p1):
-		Selection.GHOST:
+	match (Singleton.costume_p1):
+		Singleton.Selection.GHOST:
 			cursor_p1.global_position = ghost_sprite.global_position + offset 
-		Selection.KNIGHT:
+		Singleton.Selection.KNIGHT:
 			cursor_p1.global_position = knight_sprite.global_position + offset
-		Selection.PRINCESS:
+		Singleton.Selection.PRINCESS:
 			cursor_p1.global_position = princess_sprite.global_position + offset
-		Selection.WITCH:
+		Singleton.Selection.WITCH:
 			cursor_p1.global_position = witch_sprite.global_position + offset
 			
-	match (costume_p2):
-		Selection.GHOST:
+	match (Singleton.costume_p2):
+		Singleton.Selection.GHOST:
 			cursor_p2.global_position = ghost_sprite.global_position + offset 
-		Selection.KNIGHT:
+		Singleton.Selection.KNIGHT:
 			cursor_p2.global_position = knight_sprite.global_position + offset
-		Selection.PRINCESS:
+		Singleton.Selection.PRINCESS:
 			cursor_p2.global_position = princess_sprite.global_position + offset
-		Selection.WITCH:
+		Singleton.Selection.WITCH:
 			cursor_p2.global_position = witch_sprite.global_position + offset
 	
 	#shift cursor positions so they don't overlap		
-	if costume_p1 == costume_p2:
+	if Singleton.costume_p1 == Singleton.costume_p2:
 		cursor_p1.global_position = cursor_p1.global_position + x_offset_cursor1
 		cursor_p2.global_position = cursor_p2.global_position + x_offset_cursor2
--- Scripts/singleton.gd
+++ Scripts/singleton.gd
@@ -12,12 +12,7 @@ const up_p2: String = "P2_Up"
 const down_p2: String = "P2_Down"
 const confirm_p2: String = "P2_Trick"
 
-var score: int = 100
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#Used to save player's costume pick
+enum Selection { GHOST, KNIGHT, PRINCESS, WITCH }
+var costume_p1: Selection
+var costume_p2: Selection
